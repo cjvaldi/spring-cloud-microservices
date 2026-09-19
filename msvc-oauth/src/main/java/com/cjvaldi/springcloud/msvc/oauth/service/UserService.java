b62beb3886df8891707affaed10931bc21417559
CRISTIAN VALDIVIESO VALENZUELA <cjyape@hotmail.com>
2026-09-20 00:13:14 +0200

    Feat: Agregar Sección 12 - Desplegando Microservicios en Contenedores Docker - Parte 1
      - Creación de archivos Dockerfile optimizados y multi-stage para cada microservicio del ecosistema.
      - Generación de imágenes Docker y despliegue de contenedores para Config Server, Eureka Server, Zipkin y base de datos MySQL.
      - Contenerización de servicios de negocio: msvc-products, msvc-items, msvc-users, msvc-oauth y msvc-gateway-server.
      - Parametrización de variables de entorno y URLs de red interna de Docker para intercomunicación entre contenedores.
      - Verificación de escalado horizontal de instancias en msvc-products y balanceo de carga.

--- msvc-oauth/src/main/java/com/cjvaldi/springcloud/msvc/oauth/service/UserService.java
+++ msvc-oauth/src/main/java/com/cjvaldi/springcloud/msvc/oauth/service/UserService.java
@@ -7,7 +7,7 @@ import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
